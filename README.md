12efca2ca1005c4dbd64a8fb3dde0078ce7b5948
raomaster <raomaster@gmail.com>
2023-04-14 00:59:22 -0400

    addes typer for a beautiful prompt

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
-## CHATGPT API WITH PYTHON
+# CHATGPT API WITH PYTHON
 
-#First
+## First
 You need to add your api ley for openai.com
 
     export OPENAI_APY_KEY=asdasdasdasdasdadsad
 
-#References
+
+## Example
+Show the image:
+
+```
+![Example of the application](./docs/example_spanish.PNG)
+```
+
+## References
 
 - openapi library: https://github.com/openai/openai-python
 - openapi API Documentation: https://platform.openai.com/docs/api-reference/chat/create 
--- main.py
+++ main.py
@@ -1,26 +1,56 @@
 import openai
 import config
+import typer
+from rich import print
+from rich.table import Table
 
-openai.api_key = config.api_key
+def main():
+    openai.api_key = config.api_key
 
-# Context of the assistent
-messages = [{"role": "system", "content": "Eres un asistente muy util"}]
+    print("💬 [bold green]Welcome to the OpenAI Chatbot![/bold green]")
 
-while True:
+    # Create a table with commands
+    table = Table("Comando", "Descripción")
+    table.add_row("exit", "Salir de la aplicación")
+    table.add_row("new", "Crear una nueva conversación")
 
-    content = input("Escribe tu pregunta (si quieres terminar el chat escribe: salir): ")
+    print(table)
 
-    if content == "salir":
-        break
+    # Context of the assistent
+    context = [{"role": "system", "content": "Eres un asistente muy útil"}]
 
-    messages.append({"role": "user", "content": content})
+    messages = context
 
-    # Try spanish question to openai chat-gpt-api beta ref: https://platform.openai.com/docs/api-reference/chat/create 
-    response = openai.ChatCompletion.create(model = "gpt-3.5-turbo", temperature = 0.7, max_tokens = 500, top_p = 1, frequency_penalty = 0, presence_penalty = 0.6,
-                                messages = messages)
+    while True:
+
+        content = __prompt()
+
+        if content == "mew":
+            print("💬 [bold green]Nueva conversación creada[/bold green]")
+            messages = context
+            content = __prompt()
+
+        messages.append({"role": "user", "content": content})
+
+        # Try spanish question to openai chat-gpt-api beta ref: https://platform.openai.com/docs/api-reference/chat/create 
+        response = openai.ChatCompletion.create(model = "gpt-3.5-turbo", temperature = 0.7, max_tokens = 500, top_p = 1, frequency_penalty = 0, presence_penalty = 0.6,
+                                    messages = messages)
+        
+        # pass the response of the conversation to the role assistant
+        messages.append({"role": "assistant", "content": response.choices[0].message.content})
+
+        # print the response of the conversation to the console
+        print(f"[bold green]> [/bold green] [green]{response.choices[0].message.content}[/green]")
+
+def __prompt() -> str:
+    prompt = typer.prompt("\nEscribe tu pregunta")
+    # Check if the user wants to exit
+    if prompt == "exit":
+        # Confirm if the user wants to exit
+        typer.confirm("❓ ¿Seguro que quieres terminar el chat?", default=False)
+        raise typer.Abort()
     
-    # pass the response of the conversation to the role assistant
-    messages.append({"role": "assistant", "content": response.choices[0].message.content})
+    return prompt
 
-
-    print(response.choices[0].message.content)
+if __name__ == "__main__":
+    typer.run(main)
