35f9547260b54ecd6fcc7977d9d308578c4463d4
raomaster <raomaster@gmail.com>
2023-04-13 23:52:53 -0400

    coversation

--- main.py
+++ main.py
@@ -6,13 +6,21 @@ openai.api_key = config.api_key
 # Context of the assistent
 messages = [{"role": "system", "content": "Eres un asistente muy util"}]
 
-content = input("Escribe tu pregunta: ")
+while True:
 
-messages.append({"role": "user", "content": content})
+    content = input("Escribe tu pregunta (si quieres terminar el chat escribe: salir): ")
 
-# Try spanish question to openai chat-gpt-api beta ref: https://platform.openai.com/docs/api-reference/chat/create 
-response = openai.ChatCompletion.create(model = "gpt-3.5-turbo", temperature = 0.7, max_tokens = 150, top_p = 1, frequency_penalty = 0, presence_penalty = 0.6,
-                             messages = [{"role": "user", "content": content}])
+    if content == "salir":
+        break
+
+    messages.append({"role": "user", "content": content})
+
+    # Try spanish question to openai chat-gpt-api beta ref: https://platform.openai.com/docs/api-reference/chat/create 
+    response = openai.ChatCompletion.create(model = "gpt-3.5-turbo", temperature = 0.7, max_tokens = 500, top_p = 1, frequency_penalty = 0, presence_penalty = 0.6,
+                                messages = messages)
+    
+    # pass the response of the conversation to the role assistant
+    messages.append({"role": "assistant", "content": response.choices[0].message.content})
 
 
-print(response.choices[0].message.content)
+    print(response.choices[0].message.content)
